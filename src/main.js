a1c5ca28f330a2994abce5591413198d186c48ec
yuexiaogou <xiangqing0914@icloud.com>
2022-10-19 13:44:16 +0800

    引入elementui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element
+import ElementUI from 'element-ui'
+// 引入element的主题文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 将 Element 注册为 Vue插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
