da490002ba8ad01d0700d4d398d2a06362270c04
yuexiaogou <xiangqing0914@icloud.com>
2022-10-20 09:23:34 +0800

    布局容器

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h1>拉勾教育</h1>
     <!-- 根路由出口 -->
     <router-view/>
 
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <!-- 子路由出口 -->
-    <router-view></router-view>
-  </div>
+<el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -11,4 +14,18 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-container{
+height: 100vh;
+min-width: 980px;
+}
+.el-aside{
+background-color: #d3bce6;
+}
+.el-header{
+background-color: #b3c0d1;
+}
+.el-main{
+background-color: #e9eef3;
+}
+</style>
