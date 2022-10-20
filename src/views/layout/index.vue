d4fecf9e1162c28eaea7dc3a149ce330c1e65d2d
yuexiaogou <xiangqing0914@icloud.com>
2022-10-20 10:50:05 +0800

    布局处理-侧边栏菜单

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,16 +1,27 @@
 <template>
 <el-container>
-  <el-aside width="200px">Aside</el-aside>
+  <el-aside width="200px">
+<!-- 侧边栏组件 -->
+<app-aside></app-aside>
+  </el-aside>
   <el-container>
     <el-header>Header</el-header>
-    <el-main>Main</el-main>
+    <el-main>
+  <!-- 设置子路由的出口 -->
+    <router-view></router-view>
+    </el-main>
   </el-container>
 </el-container>
 </template>
 
 <script>
+// 引入侧边栏组件
+import AppAside from './components/AppAside.vue'
 export default {
-  name: 'VueLayout'
+  name: 'VueLayout',
+  components: {
+    AppAside
+  }
 }
 </script>
 
