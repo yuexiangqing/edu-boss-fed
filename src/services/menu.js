d757d89a42295bc8ddbcc1c3fb1a9642de6c3176
yuexiaogou <xiangqing0914@icloud.com>
2022-10-24 18:36:28 +0800

    权限管理-上级菜单处理

---
+++ src/services/menu.js
@@ -0,0 +1,14 @@
+import request from '@/utils/request'
+
+// 获取编辑菜单页面信息
+export const getEditMenuInfo = (id = -1) => {
+  return request({
+    method: 'GET',
+    // url: `/boss/menu/getEditMenuInfo?id=${id}`
+    // 这两个是一样的，当我们用 POST 请求时，下面用的是data
+    url: '/boss/menu/getEditMenuInfo',
+    params: {
+      id
+    }
+  })
+}
--- src/views/menu/create.vue
+++ src/views/menu/create.vue
@@ -1,10 +1,94 @@
 <template>
-    <div class="menu-create">添加菜单</div>
+    <div class="menu-create">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+        <span>添加菜单</span>
+        </div>
+        <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="菜单名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="菜单路径">
+          <el-input v-model="form.path"></el-input>
+        </el-form-item>
+        <el-form-item label="上级菜单">
+          <el-select v-model="form.parentId" placeholder="请选择上级菜单">
+            <!-- 无上级菜单 -->
+            <el-option
+              label="无上级菜单"
+              :value="-1"
+            ></el-option>
+            <!-- 选择一级菜单 -->
+            <el-option
+              v-for="item in parentMenuList"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input v-model="form.description"></el-input>
+        </el-form-item>
+        <el-form-item label="前端图标">
+          <el-input v-model="form.icon"></el-input>
+        </el-form-item>
+        <el-form-item label="是否显示">
+          <el-radio-group v-model="form.shown">
+            <!-- label 的数据会在选择后设置给 v-model 的 shown -->
+            <el-radio :label="true">是</el-radio>
+            <el-radio :label="false">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="排序">
+          <el-input-number v-model="form.orderNum" label="描述文字"></el-input-number>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">提交</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    </div>
   </template>
 
 <script>
+import { getEditMenuInfo } from '@/services/menu'
 export default {
-  name: 'MenuCreate'
+  name: 'MenuCreate',
+  data () {
+    return {
+      // 表单数据
+      form: {
+        parentId: -1,
+        name: '',
+        href: '',
+        icon: '',
+        orderNum: 5,
+        description: '',
+        shown: true
+      },
+      // 存储上级菜单数据
+      parentMenuList: []
+    }
+  },
+  created () {
+    // 加载上级菜单信息
+    this.loadMenuInfo()
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    async loadMenuInfo () {
+      // 请求上级菜单数据
+      const { data } = await getEditMenuInfo()
+      if (data.code === '000000') {
+        // 将上级菜单数据保存，进行数据绑定
+        this.parentMenuList = data.data.parentMenuList
+      }
+    }
+  }
 }
 </script>
 
