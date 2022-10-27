8335620c83839b878e08732f3346c99462d27d44
yuexiaogou <xiangqing0914@icloud.com>
2022-10-27 16:56:51 +0800

    角色管理与用户管理-分配菜单布局

---
+++ src/views/role/alloc-menu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alloc-menu">
+        这是角色ID 为:{{roleId}}
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'AllocMenu',
+  props: {
+    roleId: {
+      type: [Number, String],
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/role/components/list.vue
+++ src/views/role/components/list.vue
@@ -48,6 +48,12 @@
             <div>
               <el-button
                 type="text"
+                @click="$router.push({
+                  name: 'alloc-menu',
+                  params: {
+                    roleId: scope.row.id
+                  }
+                })"
               >分配菜单</el-button>
               <el-button
                 type="text"
