129fb9a003a6b5f1817abddf36b7c18db0eb83f4
yuexiaogou <xiangqing0914@icloud.com>
2022-10-27 17:58:42 +0800

    角色管理与用户管理-分配菜单展示

--- src/services/menu.js
+++ src/services/menu.js
@@ -36,3 +36,11 @@ export const deleteMenu = id => {
     url: `/boss/menu/${id}`
   })
 }
+
+// 获取所有菜单并安装层级展示
+export const getMenuNodeList = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getMenuNodeList'
+  })
+}
--- src/views/role/alloc-menu.vue
+++ src/views/role/alloc-menu.vue
@@ -1,16 +1,45 @@
 <template>
     <div class="alloc-menu">
-        这是角色ID 为:{{roleId}}
+        <!-- 这是角色ID 为:{{roleId}} -->
+        <el-card>
+            <el-tree
+            :data="menus"
+            :props="defaultProps"
+            default-expand-all
+            show-checkbox
+            ></el-tree>
+        </el-card>
     </div>
   </template>
 
 <script>
+import { getMenuNodeList } from '@/services/menu'
 export default {
   name: 'AllocMenu',
   props: {
     roleId: {
       type: [Number, String],
       required: true
+    }
+  },
+  created () {
+    this.loadMenus()
+  },
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'subMenuList',
+        label: 'name'
+      }
+    }
+  },
+  methods: {
+    async loadMenus () {
+      const { data } = await getMenuNodeList()
+      if (data.code === '000000') {
+        this.menus = data.data
+      }
     }
   }
 }
