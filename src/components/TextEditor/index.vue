f2fe4ff472335ca374991ff20fed6b8a0389cdba
yuexiaogou <xiangqing0914@icloud.com>
2022-10-30 19:39:27 +0800

    课程管理-图片上传处理

--- src/components/TextEditor/index.vue
+++ src/components/TextEditor/index.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import { uploadCourseImage } from '@/services/course'
 import E from 'wangeditor'
 export default {
   name: 'TextEditor',
@@ -24,6 +25,16 @@ export default {
       // 创建富文本编辑器之前，进行事件设置
       editor.config.onchange = value => {
         this.$emit('input', value)
+      }
+      // 配置自定义的图片上传功能
+      editor.config.customUploadImg = async (resultFiles, insertImgFn) => {
+        const fd = new FormData()
+        fd.append('file', resultFiles[0])
+        const { data } = await uploadCourseImage(fd)
+        if (data.code === '000000') {
+        // 通过 insertImgFn 传回给富文本编辑器进行显示
+          insertImgFn(data.data.name)
+        }
       }
       editor.create()
       editor.txt.html(this.value)
